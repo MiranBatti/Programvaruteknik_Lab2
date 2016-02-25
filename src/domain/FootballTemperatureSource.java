7fb6d93c9fcf5196ef586ced3ba8aa4b55549cc6
ndi14psd <ndi14psd@student.hig.se>
2016-02-25 13:25:40 +0100

    Hoppas det fungerar.

--- src/domain/FootballTemperatureSource.java
+++ src/domain/FootballTemperatureSource.java
@@ -1,9 +1,16 @@
 package domain;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 public class FootballTemperatureSource implements DataSource {
+	
+	private final String csvLink;
+	
+	public FootballTemperatureSource() {
+		csvLink = "http://opendata-download-metobs.smhi.se/api/version/latest/parameter/2/station/107420/period/corrected-archive/data.csv";
+	}
 
 	@Override
 	public String getName() {
@@ -17,7 +24,8 @@ public class FootballTemperatureSource implements DataSource {
 
 	@Override
 	public Map<LocalDate, Double> getData() {
-		// TODO Auto-generated method stub
+		CsvToMapParser parser = new CsvToMapParser(csvLink);
+		Map<String, Object> csvData;
 		return null;
 	}
 
