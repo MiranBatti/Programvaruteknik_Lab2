00b1d2f88ca2b630724c83c9fd8715562b939a09
MiranBatti <miran.batti@gmail.com>
2016-02-27 17:04:54 +0100

    Refactoring på getContent metoden. Använder StringBuilder istället för concat. Snabbare inläsning.

--- src/domain/UrlFetcher.java
+++ src/domain/UrlFetcher.java
@@ -23,20 +23,35 @@ public class UrlFetcher {
         }
     }
 
+//    public String getContent() {
+//        String result = "", temp = "";
+//        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+//            while (null != (temp = br.readLine())) {
+//                result += temp;
+//            }
+//        } catch (IOException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//        return result;
+//    }
+    
     public String getContent() {
-        String result = "", temp = "";
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            while (null != (temp = br.readLine())) {
-                result += temp;
-            }
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-        return result;
+    	StringBuilder result = new StringBuilder();
+    	String temp = "";
+    	try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+    		while (null != (temp = br.readLine())) {
+    			result.append(temp);
+    		}
+    	} catch (IOException ex) {
+    		throw new RuntimeException(ex);
+    	}
+    	return result.toString();
     }
     
     public static void main(String[] args) {
         System.out.println(
-                new UrlFetcher("http://api.everysport.com/v1/events?apikey=1769e0fdbeabd60f479b1dcaff03bf5c&league=63925&limit=2").getContent());
+                new UrlFetcher
+                ("http://api.everysport.com/v1/events?apikey=1769e0fdbeabd60f479b1dcaff03bf5c&league=63925&limit=2").getContent());
+        
     }
 }
